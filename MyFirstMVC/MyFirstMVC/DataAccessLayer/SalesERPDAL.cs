23032e800459d58c965025e4444b4d7d2f93644a
rakib <rakib.omar13@gmail.com>
2016-10-18 16:24:11 +0600

    MVC PRoject with DB

---
+++ MyFirstMVC/MyFirstMVC/DataAccessLayer/SalesERPDAL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using MyFirstMVC.Models;
+
+namespace MyFirstMVC.DataAccessLayer
+{
+    public class SalesERPDAL : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().ToTable("TblEmployee");
+ 	        base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- MyFirstMVC/MyFirstMVC/Models/EmployeeBusinessLayer.cs
+++ MyFirstMVC/MyFirstMVC/Models/EmployeeBusinessLayer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MyFirstMVC.DataAccessLayer;
 
 namespace MyFirstMVC.Models
 {
@@ -10,16 +11,26 @@ namespace MyFirstMVC.Models
         Random rndSalary = new Random();
         public List<Employee> GetEmployees()
         {
-            List<Employee> employees = new List<Employee>();
-            for (int i = 0; i < 10; i++) {
-                Employee emp = new Employee();
-                emp.FirstName = "johnson";
-                emp.LastName = " fernandes";
-                emp.Salary = rndSalary.Next(12000, 20000);
-                employees.Add(emp);
-            }
+
+            //List<Employee> employees = new List<Employee>();
+            //for (int i = 0; i < 10; i++) {
+            //    Employee emp = new Employee();
+            //    emp.FirstName = "johnson";
+            //    emp.LastName = " fernandes";
+            //    emp.Salary = rndSalary.Next(12000, 20000);
+            //    employees.Add(emp);
+            //}
+
+                SalesERPDAL salseDl = new SalesERPDAL();
+                return salseDl.Employees.ToList();
             
-            return employees;
+            // return employees;
+        }
+
+        public List<Employee> getEmployee()
+        {
+            SalesERPDAL salseDl = new SalesERPDAL();
+            return salseDl.Employees.ToList();
         }
     }
 }
